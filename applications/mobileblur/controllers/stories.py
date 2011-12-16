b9d6c4af939aba222cb19cce97d108ffdc80c347
Brian Cottingham <spiffytech@gmail.com>
2011-12-15 22:16:57 -0500

    Added buttons for previous and next stories in the story view

--- applications/mobileblur/controllers/stories.py
+++ applications/mobileblur/controllers/stories.py
@@ -3,9 +3,30 @@
 from pprint import pprint
 
 def view():
+    requested_story_id = request.vars["story"]
     stories = newsblur.feed(request.vars["feed_id"])["stories"]
-    story = [story for story in stories if story["id"]==request.vars["story"]][0]
-    return dict(story=story, feed_id=request.vars["feed_id"])
+    
+    previous_story = None
+    requested_story = None
+    next_story = None
+    for story in range(len(stories)):
+        if stories[story]["id"] == requested_story_id:
+            requested_story = stories[story]
+            try:
+                previous_story = stories[story+1]
+            except IndexError:
+                pass
+            if story != 0:
+                next_story = stories[story-1]
+
+            break
+
+    return dict(
+        previous_story=previous_story,
+        requested_story=requested_story, 
+        next_story=next_story,
+        feed_id=request.vars["feed_id"]
+    )
 
 def mark_read():
     results = newsblur.mark_story_as_read(request.vars["story_id"], request.vars["feed_id"])
