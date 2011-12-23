559b33573c0b24af82ce62c2b1599245c2d4be37
Brian Cottingham <spiffytech@gmail.com>
2011-12-23 17:28:49 -0500

    Making the fabfile push to github is an important step of releasing an update

--- fabfile.py
+++ fabfile.py
@@ -1,25 +1,30 @@
 from fabric.api import *
 from fabric.contrib.console import confirm
+from fabric.operations import sudo
 
 env.hosts = ["spiffytech@short.csc.ncsu.edu"]
 
+def test():
+    print local("git flow help")
+
 def release(version):
     if local("git branch | grep '*'") != "* release/%s" % version:
-        local("git flow release start %s" % version)
+        print local("git flow release start %s" % version)
         if not confirm("A release has been started and staged locally. Does it behave like it should?"):
             abort("Aborting...")
     else:
         print "Already on branch release/%s" % version
 
-    local("git flow release finish %s" % version)
+    print local("git flow release finish %s" % version)
+    print local("git push github master")
 
     with cd("apache/mobileblur.spiffyte.ch/docroot"):
-        run("git pull")
+        print run("git pull")
         with settings(warn_only = True):
             result = run("httpd -t")
             if result.failed and not ("Apache has errors. Continue anyway?"):
                 abort("Aborting...")
 
-            result = run("sudo service httpd restart")
+            result = sudo("service httpd restart")
             if result.failed and confirm ("Apache didn't start up again! Revert to last release?"):
-                run("git reset --hard HEAD^")
+                print run("git reset --hard HEAD^")
