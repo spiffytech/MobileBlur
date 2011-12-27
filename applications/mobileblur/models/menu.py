b7529bf27de834ba40b1708c6591796e379155bb
Brian Cottingham <spiffytech@gmail.com>
2011-12-26 23:39:19 -0500

    Changed some site metadata from the web2py defaults to real values

--- applications/mobileblur/models/menu.py
+++ applications/mobileblur/models/menu.py
@@ -8,11 +8,11 @@ response.title = request.application
 response.subtitle = T('customize me!')
 
 #http://dev.w3.org/html5/markup/meta.name.html
-response.meta.author = 'you'
-response.meta.description = 'Free and open source full-stack enterprise framework for agile development of fast, scalable, secure and portable database-driven web-based applications. Written and programmable in Python'
-response.meta.keywords = 'web2py, python, framework'
+response.meta.author = 'spiffytech'
+response.meta.description = 'Mobile-friendly interface for the Newsblur RSS reader'
+response.meta.keywords = 'mobileblur,newsblur,mobile rss'
 response.meta.generator = 'Web2py Enterprise Framework'
-response.meta.copyright = 'Copyright 2007-2010'
+response.meta.copyright = 'Copyright 2011+'
 
 
 ##########################################
