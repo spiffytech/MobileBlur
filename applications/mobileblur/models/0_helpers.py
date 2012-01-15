493e7e49e80b2ad9315bc29fb4198ad6ad18b641
Brian Cottingham <spiffytech@gmail.com>
2012-01-15 14:48:59 -0500

    Fixed bug that prevented the error page from showing up when you tried to log in while Newsblur was down

--- applications/mobileblur/models/0_helpers.py
+++ applications/mobileblur/models/0_helpers.py
@@ -18,7 +18,7 @@ else:
     threshold = int(request.cookies["threshold"].value)
 thresholds = ["nt", "ps", "ng"]  # indices -1, 0, 1 for negative, neutral, and positive intelligence filters
 
-if [request.application, request.controller, request.function] != [request.application, "default", "login"]:
+if [request.application, request.controller, request.function] not in [[request.application, "default", "login"], [request.application, "errors", "index"]]:
     if "nb_cookie" not in request.cookies.keys():
         redirect(URL("default", "login"))
     else:
