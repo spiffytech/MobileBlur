73fa55106922ea23868de473a9c3a30e4a9a5ad4
Brian Cottingham <spiffytech@gmail.com>
2019-02-17 12:46:18 -0500

    Enable infinite scroll for the story list. Also, change Windows line endings to Linux style.

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,18 +1,17 @@
 <template>
-    <div v-if="loading">Loading...</div>
-    <div v-else>
-        <h1 class="title">{{feed.feed_title}}</h1>
-        <div
-            v-for="story in this.stories"
-            :key="story.id"
-            :class="{read: story.read_status === 1}"
-        >
-            <router-link :to="{name: 'story', params: {feed: feed.id, story: story.id, page: story.page}}">
-                <header>{{story.story_title}}</header>
-                <p>{{story.long_parsed_date}}</p>
-            </router-link>
-        </div>
+  <div v-if="loading">Loading...</div>
+  <div v-else>
+    <h1 class="title">{{feed.feed_title}}</h1>
+    <div v-for="story in this.stories" :key="story.id" :class="{read: story.read_status === 1}">
+      <router-link
+        :to="{name: 'story', params: {feed: feed.id, story: story.id, page: story.page}}"
+      >
+        <header>{{story.story_title}}</header>
+        <p>{{story.long_parsed_date}}</p>
+      </router-link>
     </div>
+    <div v-if="noMorePages">End of the line</div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -21,31 +20,51 @@ import Vue from 'vue';
 import * as newsblur from '../lib/newsblur';
 
 export default Vue.extend({
-   data() {
-       return {
-           lastPage: 0,
-           loading: false,
-           feed: null,
-           stories: [] as any[],
-       };
-   },
-   async beforeMount() {
-       this.loading = true;
-       await this.loadStories();
-       this.loading = false;
-   },
+  data() {
+    return {
+      lastPage: 0,
+      loading: false,
+      feed: null,
+      stories: [] as any[],
+      noMorePages: false,
+    };
+  },
+  async beforeMount() {
+    this.loading = true;
+  },
 
-   methods: {
-       async loadStories() {
-        const currentPage = this.lastPage + 1;
-        const response =
-            await newsblur.fetchStories(this.$route.params.feed, currentPage);
-        if (!this.feed) this.feed = response;
-        const storiesWithPageNumber =
-            response.stories.map((story: any) => ({...story, page: currentPage}));
-        this.stories = this.stories.concat(storiesWithPageNumber);
-        this.lastPage = currentPage;
-       },
-   },
+  mounted() {
+    this.scroll();
+  },
+
+  methods: {
+    async loadStories() {
+      if (this.noMorePages) return;
+      const currentPage = this.lastPage + 1;
+      const response = await newsblur.fetchStories(
+        this.$route.params.feed,
+        currentPage,
+      );
+      if (!this.feed) this.feed = response;
+      const storiesWithPageNumber = response.stories.map((story: any) => ({
+        ...story,
+        page: currentPage,
+      }));
+      this.stories = this.stories.concat(storiesWithPageNumber);
+      this.noMorePages = response.stories.length === 0;
+      console.log(response.stories.length);
+      this.lastPage = currentPage;
+      this.loading = false;
+    },
+
+    async scroll() {
+      while (true) {
+        const bottomOfWindow =
+          window.innerHeight + window.scrollY >= document.body.offsetHeight;
+        if (bottomOfWindow) await this.loadStories();
+        await new Promise((resolve, reject) => setTimeout(resolve, 100));
+      }
+    },
+  },
 });
 </script>
